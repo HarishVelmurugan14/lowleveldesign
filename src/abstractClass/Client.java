99be55978b46961c372195af37b173285b566b4d
harish.vk <harish.vk@zohocorp.com>
2025-10-08 13:49:36 +0530

    Abstract class

--- src/abstractClass/Client.java
+++ src/abstractClass/Client.java
@@ -4,6 +4,13 @@ public class Client
 {
     public static void main(String[] args)
     {
+
+        // HK NOTES :
+        // Abstract class does not instantiate an object instead some child class extends it and uses its method
+        // Child class can override normal methods but absolutely override abstract methods
+
+        // Bird bird = new Bird(); COMPILE TIME ISSUE
+
          Bird bird = new Sparrow();
          bird.fly();
     }
