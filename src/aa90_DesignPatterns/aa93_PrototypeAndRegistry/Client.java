b436fbbd18427827c04fbc6235422b33f7aff2d4
harish.vk <harish.vk@zohocorp.com>
2025-10-28 18:39:23 +0530

    Prototype DP

--- src/aa90_DesignPatterns/aa93_PrototypeAndRegistry/Client.java
+++ src/aa90_DesignPatterns/aa93_PrototypeAndRegistry/Client.java
@@ -7,16 +7,17 @@ import aa90_DesignPatterns.aa93_PrototypeAndRegistry.registry.Human;
 import aa90_DesignPatterns.aa93_PrototypeAndRegistry.registry.HumanRegistry;
 import aa90_DesignPatterns.aa93_PrototypeAndRegistry.registry.RaceType;
 
-public class Client
-{
+public class Client {
     public static void main(String[] args) {
+
+        //----------------------------------------------------------------------------------------
         Student student = new Student("Rohit", 23, 57);
 
         IntelligentStudent intelligentStudent
                 = new IntelligentStudent("Intelligent Rohit", 23, 90, 105);
-
-        StudentUtil.doCopy(student);
-        StudentUtil.doCopy(intelligentStudent);
+        Student s1 = prototypePattern(student);
+        Student s2 = prototypePattern(intelligentStudent);
+        //----------------------------------------------------------------------------------------
 
         HumanRegistry humanRegistry = new HumanRegistry();
 
@@ -32,5 +33,23 @@ public class Client
         System.out.println(masterElf);
         System.out.println(masterOrc);
         System.out.println(masterZombie);
+    }
+
+    private static Student prototypePattern(Student student) {
+        // NOTE : PROPER WAY
+        StudentUtil.doCopy(student);
+        // Solution :  By making a copy method and calling copy constructor in it makes this easier
+        // if student is plain / student is intelligent runtime polymorphism takes care of this and calls equivalent copy method
+
+        // Problem : break SRP & OCP : it has too many responsibilities
+        // and for every inherited class we need to affect this if look
+        // NOTE : OLD WAY
+        if (student instanceof Student) {
+            return new Student(student);
+        } else if (student instanceof IntelligentStudent) {
+            return new IntelligentStudent((IntelligentStudent) student);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
