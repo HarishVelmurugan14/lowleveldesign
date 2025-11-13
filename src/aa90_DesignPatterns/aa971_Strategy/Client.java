b19d03fff2982c003e97ff6f3ef368a140294e75
harish.vk <harish.vk@zohocorp.com>
2025-11-13 08:12:23 +0530

    Strategy Pattern

--- src/aa90_DesignPatterns/aa971_Strategy/Client.java
+++ src/aa90_DesignPatterns/aa971_Strategy/Client.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Client
 {
     public static void main(String[] args) {
+        // High Fly, Low Fly birds :  take red wing and tried to fly high
         System.out.println("Please enter a strategy for logging in");
         Scanner scanner = new Scanner(System.in);
         LoginStrategy loginStrategy = LoginStrategyFactory.getLoginStrategy(scanner.next());
